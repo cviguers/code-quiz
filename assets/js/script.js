e09bb6cd28bc33c441c2d1ff04136c8043c2f1dc
cviguers <cviguers@gmail.com>
2023-03-14 17:39:40 -0400

    styling complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // js created elements
-var main = document.main;
 var questionContainer = document.createElement("div");
 var question = document.createElement("h2");
 var btnContainer = document.createElement("div");
@@ -25,13 +24,21 @@ answer3.textContent = "answer three";
 answer4.textContent = "answer four";
 
 // append js elements to html elements
-main.appendChild(questionContainer);
+mainEl.appendChild(questionContainer);
 questionContainer.appendChild(question);
 questionContainer.appendChild(btnContainer);
 btnContainer.appendChild(answer1);
 btnContainer.appendChild(answer2);
 btnContainer.appendChild(answer3);
 btnContainer.appendChild(answer4);
+
+// styles js elements
+questionContainer.setAttribute("style"," margin:auto; width:50%; text-align:center; ")
+answer1.setAttribute("style", " color: #cbece0; background: #e0cbec; border-radius: 100px; font-size: 18px; padding: 7px 20px; border: 0; margin: 50px; font-weight:bold; ");
+answer2.setAttribute("style", " color: #cbece0; background: #e0cbec; border-radius: 100px; font-size: 18px; padding: 7px 20px; border: 0; margin: 50px; font-weight:bold; ");
+answer3.setAttribute("style", " color: #cbece0; background: #e0cbec; border-radius: 100px; font-size: 18px; padding: 7px 20px; border: 0; margin: 50px; font-weight:bold; ");
+answer4.setAttribute("style", " color: #cbece0; background: #e0cbec; border-radius: 100px; font-size: 18px; padding: 7px 20px; border: 0; margin: 50px; font-weight:bold; ");
+
 
 // start timer
 function setTime() {
